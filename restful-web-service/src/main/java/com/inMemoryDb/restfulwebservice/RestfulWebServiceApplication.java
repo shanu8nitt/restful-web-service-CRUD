f5143fb7a5143bf6732b4581377831cab1464be4
SHASUMAN.ORADEV <shanu.k.suman@oracle.com>
2019-06-23 18:20:20 +0530

    Adding Translation funtionality

--- restful-web-service/src/main/java/com/inMemoryDb/restfulwebservice/RestfulWebServiceApplication.java
+++ restful-web-service/src/main/java/com/inMemoryDb/restfulwebservice/RestfulWebServiceApplication.java
@@ -1,13 +1,38 @@
 package com.inMemoryDb.restfulwebservice;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServiceApplication.class, args);
-	}
+			public static void main(String[] args) {
+				SpringApplication.run(RestfulWebServiceApplication.class, args);
+			}
+
+			@Bean
+			public LocaleResolver localeResolver() {
+				SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+				localeResolver.setDefaultLocale(Locale.US);
+				return localeResolver;
+				
+			}
+			
+			
+			//@Bean({"b1", "b2"}) 
+			@Bean
+			public ResourceBundleMessageSource messageSource()
+			{
+				ResourceBundleMessageSource messageSource_samename = new ResourceBundleMessageSource();
+				messageSource_samename.setBasename("TranslationFile");
+				return messageSource_samename;
+				
+			}
 
 }
--- restful-web-service/src/main/java/com/inMemoryDb/restfulwebservice/helloworld/HelloWorldController.java
+++ restful-web-service/src/main/java/com/inMemoryDb/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.inMemoryDb.restfulwebservice.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource9 ;
+	
+	
 	@GetMapping("/hello")
 	public hellowordbean name() {
 		return new hellowordbean("Hello shanu, how are you doing");
@@ -15,6 +25,12 @@ public class HelloWorldController {
 	@GetMapping("/hello/{naam}")  //  -> /hello/anything
 	public hellowordbean naam(@PathVariable String naam) {
 		return new hellowordbean(naam);
+		
+	}
+	
+	@GetMapping("/hello-translation")
+	public String name(@RequestHeader(name = "Accept-Language", required = false) Locale local) {
+		return messageSource9.getMessage("HELLO_LABEL", null, local);
 		
 	}
 	
